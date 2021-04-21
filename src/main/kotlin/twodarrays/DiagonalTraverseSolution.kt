9b1bee8c732387c21a4d5bfc4961fbdc305fab14
Dmitrii Podlesnykh <podlesnykh.dmitrii@gmail.com>
2021-04-21 05:59:23 +0300

    rename variables

--- src/main/kotlin/twodarrays/DiagonalTraverseSolution.kt
+++ src/main/kotlin/twodarrays/DiagonalTraverseSolution.kt
@@ -20,43 +20,50 @@ package twodarrays
 class DiagonalTraverseSolution {
 
     fun findDiagonalOrder(matrix: Array<IntArray>): IntArray {
+        if (matrix.isEmpty()) {
+            return intArrayOf()
+        }
+        return findDiagonalOrderFromNonEmptyMatrix(matrix)
+    }
+
+    private fun findDiagonalOrderFromNonEmptyMatrix(matrix: Array<IntArray>): IntArray {
         val lastXIndex = matrix[0].size
         val lastYIndex = matrix.size
         val result = IntArray(lastXIndex * lastYIndex)
 
         var isStartFromTop = false
-        var resultNextElement = 0
-        for (i in 0 until lastYIndex) {
+        var resultNextElementIndex = 0
+        for (currentDiagonalIndex in 0 until lastYIndex) {
             if (isStartFromTop) {
-                var j = 0
-                while (j <= i) {
-                    result[resultNextElement] = matrix[i + j - 1][i - j]
-                    resultNextElement++
-                    j++
+                var currentPositionInDiagonalIndex = 0
+                while (currentPositionInDiagonalIndex <= currentDiagonalIndex) {
+                    result[resultNextElementIndex] = matrix[currentDiagonalIndex + currentPositionInDiagonalIndex - 1][currentDiagonalIndex - currentPositionInDiagonalIndex]
+                    resultNextElementIndex++
+                    currentPositionInDiagonalIndex++
                 }
             } else {
-                var j = i
+                var j = currentDiagonalIndex
                 while (j >= 0) {
-                    result[resultNextElement] = matrix[j][i - j]
-                    resultNextElement++
+                    result[resultNextElementIndex] = matrix[j][currentDiagonalIndex - j]
+                    resultNextElementIndex++
                     j--
                 }
             }
             isStartFromTop = !isStartFromTop
         }
-        for (i in lastYIndex - 1 downTo 0) {
+        for (i in lastYIndex downTo 2) {
             if (isStartFromTop) {
-                var j = 0
-                while (j < i) {
-                    result[resultNextElement] = matrix[i + j - 1][i - j]
-                    resultNextElement++
+                var j = 1
+                while (j <= lastXIndex-1) {
+                    result[resultNextElementIndex] = matrix[lastYIndex + j - i][lastXIndex-j]
+                    resultNextElementIndex++
                     j++
                 }
             } else {
                 var j = i
-                while (j > 0) {
-                    result[resultNextElement] = matrix[lastYIndex - i][lastXIndex - j]
-                    resultNextElement++
+                while (j > 1) {
+                    result[resultNextElementIndex] = matrix[lastYIndex - i +1][lastXIndex - j +1]
+                    resultNextElementIndex++
                     j--
                 }
             }
--- src/test/kotlin/twodarrays/DiagonalTraverseSolutionTest.kt
+++ src/test/kotlin/twodarrays/DiagonalTraverseSolutionTest.kt
@@ -32,4 +32,58 @@ internal class DiagonalTraverseSolutionTest {
         assertArrayEquals(intArrayOf(1, 2, 3, 4), sut.findDiagonalOrder(request))
     }
 
+    /**
+     * Custom example
+     *
+     * matrix [[]]
+     *
+     * response []
+     */
+    @Test
+    fun findDiagonalOrder_emptyMatrix_returnEmptyArray() {
+
+        assertArrayEquals(intArrayOf(), sut.findDiagonalOrder(arrayOf()))
+    }
+
+    /**
+     * Custom example
+     */
+    @Test
+    fun findDiagonalOrder_oneLineMatrix_returnCorrectArray() {
+        val request: Array<IntArray> = arrayOf(intArrayOf(1, 2))
+        assertArrayEquals(intArrayOf(1, 2), sut.findDiagonalOrder(request))
+    }
+
+    /**
+     * Custom example
+     */
+    @Test
+    fun findDiagonalOrder_oneLineThreeElementsMatrix_returnCorrectArray() {
+        val request: Array<IntArray> = arrayOf(intArrayOf(1, 2, 3))
+        assertArrayEquals(intArrayOf(1, 2, 3), sut.findDiagonalOrder(request))
+    }
+
+    @Test
+    fun findDiagonalOrder_oneColumnMatrix_returnCorrectArray() {
+        val request: Array<IntArray> = arrayOf(
+            intArrayOf(1),
+            intArrayOf(2)
+        )
+        assertArrayEquals(intArrayOf(1, 2), sut.findDiagonalOrder(request))
+    }
+
+    @Test
+    fun findDiagonalOrder_4x4_returnCorrectArray() {
+        val request: Array<IntArray> = arrayOf(
+            intArrayOf(1, 2, 3, 4),
+            intArrayOf(5, 6, 7, 8),
+            intArrayOf(9, 10, 11, 12),
+            intArrayOf(13, 14, 15, 16),
+        )
+
+        assertArrayEquals(
+            intArrayOf(1, 2, 5, 9, 6, 3, 4, 7, 10, 13, 14, 11, 8, 12, 15, 16),
+            sut.findDiagonalOrder(request))
+    }
+
 }
